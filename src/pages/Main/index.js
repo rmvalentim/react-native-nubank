b559fbd4f698842786442bfd127dfa34f0bacb35
Rafael Valentim <rmoraesvalentim@gmail.com>
2019-08-20 14:53:54 -0300

    Static interface finalized

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
   return (
@@ -12,6 +17,25 @@ export default function Main() {
       <StatusBar  barStyle="light-content" backgroundColor="#8B10AE"/>
       <Container>
         <Header />
+        <Content>
+          <Menu />
+          <Card>
+            <CardHeader> 
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader> 
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de João da Silva hoje as 06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </Content>
+
         <Tabs />
       </Container>
     </>
